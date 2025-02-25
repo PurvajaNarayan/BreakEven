8e485a3c9a2f3ad2d77b840710737a15f5fe4c43
PurvajaNarayan <purvajanarayana@gmail.com>
2025-02-25 12:43:43 -0500

    Initial setup with login and signup page

---
+++ root/frontEnd/break_even/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import UserRegistration from './components/userRegistration';
+
+function App() {
+  return (
+    <UserRegistration/>
+    
+  );
+}
+
+export default App;
